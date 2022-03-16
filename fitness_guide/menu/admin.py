d2fdf997a115b2b91f20344dc79c07a045d70983
tunsmm <dragon553180@gmail.com>
2022-03-16 15:18:06 +0300

    Add static in menu. Add admin and first views

--- fitness_guide/menu/admin.py
+++ fitness_guide/menu/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Client
+
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ("pk", "full_name", "sex", "type_diet", "sport_on_week", "no_eats_days_per_week", "eats_per_day")
+    search_fields = ("type_diet", "sex",)
+    list_filter = ("sport_on_week", "no_eats_days_per_week", "eats_per_day",)
+    empty_value_display = "-пусто-"
+
+
+admin.site.register(Client, ClientAdmin)
+
--- fitness_guide/menu/views.py
+++ fitness_guide/menu/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+
+from .models import Client
+
 # Create your views here.
+
+"""
+def index(request):
+    # minus - sorts from largest to smallest
+    latest = Client.objects.order_by('-phone_number')[:10]
+    output = []
+    for item in latest:
+        output.append(item.full_name)
+    return HttpResponse('\n'.join(output))
+"""  
+
+def index(request):
+    latest = Client.objects.order_by('-phone_number')[:10]
+    return render(request, "index.html", {"clients":latest})
