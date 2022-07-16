95d7d04c6aae06c60bba6bcbc4e54cd792495755
tunsmm <dragon553180@gmail.com>
2022-07-16 11:32:22 +0300

    Add some stuff for debug etc...

--- fitness_guide/fitness_guide/urls.py
+++ fitness_guide/fitness_guide/urls.py
@@ -13,14 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import handler404, handler500 
+from django.conf import settings
+from django.conf.urls import handler404, handler500
 from django.contrib import admin
 from django.urls import include, path
+import debug_toolbar
 
 
 handler404 = "menu.views.page_not_found"  # noqa
 handler500 = "menu.views.server_error"  # noqa
-
 
 urlpatterns = [
     path("", include("menu.urls")),  # main page
@@ -28,4 +29,13 @@ urlpatterns = [
     path("auth/", include("users.urls")),  # registration and autorization
     path("auth/", include("django.contrib.auth.urls")),  # if in users.urls don't exist this template
     path('about/', include('django.contrib.flatpages.urls')),
+    
 ]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns = [
+        path(r'^__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
